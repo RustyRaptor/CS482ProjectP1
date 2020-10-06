48d9469dff7bf6429f212f37385f47f147359973
RustyRaptor <ziada@nmsu.edu>
2020-10-05 22:01:42 -0600

    moved connection variable to the main segment

--- part2.py
+++ part2.py
@@ -30,10 +30,8 @@ def create_connection(host_name, user_name, user_password):
     return connection
 
 
-# This initiates the connection to the DB on the CS machines.
-# To use on your own instance replace the user_name and insert a password
-# DO NOT push this file to github with your password in here. Make sure you remove it first.
-connection = create_connection("dbclass.cs.nmsu.edu", "zarafat", "")
+
+
 
 
 # Command line arguments can be passed and read from sys.argv as a python list of strings
@@ -41,3 +39,8 @@ if __name__ == "__main__":
     print(f"Arguments count: {len(sys.argv)}")
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
+    
+    # This initiates the connection to the DB on the CS machines.
+    # To use on your own instance replace the user_name and insert a password
+    # DO NOT push this file to github with your password in here. Make sure you remove it first.
+    connection = create_connection("dbclass.cs.nmsu.edu", "zarafat", "")
