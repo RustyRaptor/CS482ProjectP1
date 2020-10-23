3546496ab6cc4e9082cb7bc4cd3cb54e60f350cb
RustyRaptor <ziada@nmsu.edu>
2020-10-23 12:56:29 -0600

    added a tests script

---
+++ tests.sh
@@ -0,0 +1,9 @@
+
+mysql -u user -p zarafat_482502fa20 < db.sql
+
+python3 part2.py 1 "Peruvian Paso" && # non existant street
+python3 part2.py 1 "Streamwood" && # existing street
+python3 part2.py 5 &&
+python3 part2.py 6 1003 &&
+python3 part2.py 7 &&
+python3 part2.py 8
