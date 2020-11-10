e10d268d0c4f7159184ac0bcc37754223312e126
Brianna Moffett <bmoffett@dijkstra.cs.nmsu.edu>
2020-11-10 15:43:13 -0700

    added connection success message for testing

--- php/index.php
+++ php/index.php
@@ -1,12 +1,15 @@
 <?php
 $servername = "dbclass.cs.nmsu.edu";
-$username = "bmoffe";
+$username = "bmoffett";
 $password = "7z4Yv0bJ";
 $dbname = "MariaDB";
 
 $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error){
 	die("Connection failed. Reason: " . $conn->connect_error);
+}
+else{
+	echo "Connection sucessful!";
 }
 $conn->close();
 ?>
