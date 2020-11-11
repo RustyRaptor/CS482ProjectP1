0ce61f4d2e2d948dab0aeb1dd00890b95ea4f534
Brianna Moffett <bmoffett@poobah.cs.nmsu.edu>
2020-11-11 16:33:17 -0700

    created index.html for form submission

--- php/index.php
+++ php/index.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "dbclass.cs.nmsu.edu";
-$username = "bmoffett";
-$password = "7z4Yv0bJ";
-$dbname = "MariaDB";
+$servername = servername;
+$username = username;
+$password = password;
+$dbname = dbname;
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
-	die("Connection failed. Reason: " . $conn->connect_error);
-}
-else{
-	echo "Connection sucessful!";
+        die("Connection failed. Reason: " . $conn->connect_error);
 }
 $conn->close();
+?>
