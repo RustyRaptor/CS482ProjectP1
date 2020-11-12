24b5ca351368cf46ce0523e553f0aecb68307248
Brianna Moffett <bmoffett@poobah.cs.nmsu.edu>
2020-11-11 17:01:01 -0700

    index.php now correctly reads data from html form

--- php/index.php
+++ php/index.php
@@ -1,12 +1,20 @@
+<html>
+<body>
+<title>ABC media</title>
 <?php
-$servername = echo $_POST["servername"];
-$username = echo $_POST["username"];
-$password = echo $_POST["password"];
-$dbname = echo $_POST["dbname"];
+$servername = $_GET["servername"];
+$username = $_GET["username"];
+$password = $_GET["password"];
+$dbname = $_GET["dbname"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
         die("Connection failed. Reason: " . $conn->connect_error);
 }
+else{
+	echo "Welcome to the database " . $username;
+}
 $conn->close();
 ?>
+</body>
+</html>
