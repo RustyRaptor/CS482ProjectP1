03900b33e6661b884499d8fb035f14f4ea55cddb
Brianna Moffett <bmoffett@poobah.cs.nmsu.edu>
2020-11-11 18:15:31 -0700

    digital display query working (no modelNo click yet

--- php/index.php
+++ php/index.php
@@ -13,8 +13,30 @@ if ($conn->connect_error){
 }
 else{
 	echo "Welcome to the database " . $username;
+	echo nl2br("\n");
 }
-$conn->close();
+?>
+<h3>Displays</h3>
+<?php
+	$query = "select * from DigitalDisplay;";
+	$result = $conn->query($query) or die(mysqli_error($conn));
+?>
+<?php
+		if($result->num_rows > 0){
+			while($r = $result->fetch_assoc()){
+?>
+<tr>
+				<td><?php echo $r["serialNo"];?></td>
+				<td><?php echo $r["schedulerSystem"];?></td>
+				<td><?php echo $r["modelNo"];?></td>
+				<td><?php echo nl2br("\n");?></td>
+</tr>
+<?php
+			}
+		}
+		else{
+			echo "No displays to show.";
+		}
 ?>
 </body>
 </html>
