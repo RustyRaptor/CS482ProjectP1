f34c07773d188e417aa5f9d9f1ccca57556ee920
Brianna Moffett <bmoffett@poobah.cs.nmsu.edu>
2020-11-12 03:37:30 -0700

    button click works and redirects, cannot append modelNo to end of query string

--- php/index.php
+++ php/index.php
@@ -20,40 +20,54 @@ else{
 <?php
 	$query = "select * from DigitalDisplay;";
 	$result = $conn->query($query) or die(mysqli_error($conn));
-?>
-<?php
 		if($result->num_rows > 0){
 			while($r = $result->fetch_assoc()){
 ?>
 <tr>
 				<td><?php echo $r["serialNo"];?></td>
 				<td><?php echo $r["schedulerSystem"];?></td>
-				<form action="" method="POST">
-				<td><input type="submit" name"modelNo" value="<?php echo $r["modelNo"];?>"/><br></td>
-				</form>
+				<td><?php echo $r["modelNo"];?></td>
 				<td><?php echo nl2br("\n");?></td>
 </tr>
 <?php
 			}
 		}
-		if($_SERVER['REQUEST_METHOD'] === 'POST'){
-			if(isset($_POST['modelNo'])){
-				$aquery = "select * from Model;";
-				$qresult = $conn->aquery($aquery) or die(mysqli_error($conn));
-				while($qr = $qresult->fetch_assoc()){
-?>					<h3>Information for selected model number:</h3>
-					<td><?php echo $r["modelNo"];?></td>
-					<td><?php echo $r["width"];?></td>
-					<td><?php echo $r["height"];?></td>
-					<td><?php echo $r["depth"];?></td>
-					<td><?php echo $r["screenSize"];?></td>
+?>
+<h2>Click on a model number below to show that model number's information.</h2>
 <?php
-				}
-			}
+	$query2 = "select modelNo from DigitalDisplay;";
+	$result2 = $conn->query($query2) or die(mysqli_error($conn));
+	#$query3 = "select * from Model where modelNo=" . $r2["modelNo"];
+	#$result3 = $conn->query($query3) or die(mysqli_error($conn));
+	if($result2->num_rows > 0){
+		while($r2 = $result2->fetch_assoc()){
+?>
+<tr>
+		<form action="model_page.php" method="get">
+			<td><input type="submit" value="<?php echo $r2["modelNo"];?>"</button></td>
+		</form>
+</tr>
+<?php
 		}
-		else{
-			echo "sorry buddy";
-		}
+	}
+	#if(isset($_POST["modelNo"])){
+	#	echo "i am here";
+	#	$query3 = "select * from Model where modelNo=" . $result2["modelNo"] . ";";
+	#	$result3 = $conn->query($query3) or die(mysqli_error($conn));
+	#	if($result3->num_rows > 0){
+	#		while($r3 = $result3->fetch_assoc()){
+?>
+<tr>
+				<td><?php echo $r3["modelNo"];?></td>
+                                                <td><?php echo $r3["width"];?></td>
+                                                <td><?php echo $r3["height"];?></td>
+                                                <td><?php echo $r3["weight"];?></td>
+						<td><?php echo $r3["screenSize"];?></td>
+</tr>
+<?php
+#			}
+#		}
+#	}
 ?>
 </body>
 </html>
